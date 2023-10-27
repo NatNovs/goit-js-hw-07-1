faa15c88fc5d5984a314e0bd7fdf6c04539775d6
NatNovs <136241094+NatNovs@users.noreply.github.com>
2023-10-27 20:43:21 +0200

    3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,5 +3,30 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const galleryList = document.querySelector('.gallery');
+const cardsMarcup = cardsGallery(galleryItems);
 
-1
+galleryList.insertAdjacentHTML('beforeend', cardsMarcup);
+
+galleryList.addEventListener('click', onGalleryContainerClick);
+
+
+function cardsGallery (gallery) {
+    return galleryItems
+    .map((img) => {
+return `
+<div class="gallery__item">
+<a class="gallery__link" href=${img.original}>
+    <img
+    class="gallery__image"
+    src=${img.preview}
+    data-source=${img.original}
+    alt=${img.description}
+    />
+</a>
+</div>
+`;
+})
+.join('');
+}
+
