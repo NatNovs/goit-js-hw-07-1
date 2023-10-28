a306a46f9cd41421a9b462597d30af3fca17161c
NatNovs <136241094+NatNovs@users.noreply.github.com>
2023-10-28 16:18:43 +0200

    Update 02-lightbox.js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,38 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryList = document.querySelector(".gallery");
+const cardsItem = createGalleryCards(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", cardsItem);
+
+function createGalleryCards(galleryList) {
+    return galleryList
+    .map(({ preview, original, description }) => {
+return `<li>
+<a class="gallery__item" href="${original}">
+<img 
+    class="gallery__image"
+    src="${preview}"
+    alt="${description}" 
+    />
+    </a>
+</li>`;
+    })
+    .join("");
+}
+
+
+let lightbox = new SimpleLightbox(".gallery__item", { 
+    captionPosition: "button",
+    captionsData: "alt",
+    disableRightClick: true,
+    captionDelay: 250,
+    fadeSpeed: 500,
+});
+
+console.log(galleryList);
+console.log(lightbox);
+
